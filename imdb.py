27d5a373b45f255dc2d7a76a19ceeb572b104485
Xioustic <github@xioustic.com>
2018-09-01 09:53:42 -0700

    fixed imdb, output json if main

--- imdb.py
+++ imdb.py
@@ -1,4 +1,38 @@
 from pyquery import PyQuery as pq
+import json
+
+def parseIMDBlist(url):
+    results = []
+
+    d = pq(url=url)
+    movies = d('div#pagecontent div#main div.lister-list div.lister-item.mode-advanced')
+    for movie in movies.items():
+        movie = pq(movie)
+        # print "=----------------------------="
+        obj = {}
+        #print movie('.lister-item-header .text-primary').text()
+        #print movie.children('.lister-item-header').text()
+        
+        obj['rank'] = int(movie('.lister-item-header span.text-primary').text()[:-1])
+
+        link = movie('.lister-item-header > a')
+        obj['title'] = link.text()
+        obj['imdb_id'] = link.attr['href'].split('/')[2]
+
+        year_text = movie('.lister-item-header > .text-muted').text().split(' ')[-1].replace('(','').replace(')','')
+        obj['year'] = int(year_text)
+
+        obj['outline'] = movie('p.text-muted').eq(1).text()
+        obj['genres'] = movie('p.text-muted').eq(0).text().split(' | ')[-1].split(', ')
+        obj['rated'] = movie('p.text-muted').eq(0).text().split(' | ')[0]
+        obj['runtime'] = movie('p.text-muted').eq(0).text().split(' | ')[1]
+        obj['rating'] = movie('.ratings-imdb-rating > strong').text().replace('.','')
+        obj['numratings'] = movie('.sort-num_votes-visible > span').eq(1).text().replace(',','')
+
+        #print obj
+        results.append(obj)
+            
+    return results
 
 def getTop100ByYear(year):
     url1 = 'http://www.imdb.com/search/title?sort=moviemeter,asc&start=1&title_type=feature&year=%d,%d' % (year,year)
@@ -7,68 +41,24 @@ def getTop100ByYear(year):
     results = []
     
     for url in [url1,url2]:
-        d = pq(url=url)
-        movies = d('tr.detailed')
-        for movie in movies.items():
-            # print movie
-            # print "=----------------------------="
-            obj = {}
-            obj['rank'] = int(movie.children('.number').text()[:-1])
-            z = movie.children('td.title > a')
-            obj['title'] = z.text()
-            obj['imdb_id'] = z.attr['href'][7:-1]
-            obj['title'] = movie.children('td.title > a').text()
-            obj['year'] = int(movie.children('td.title > span.year_type').text()[1:-1])
-            obj['outline'] = movie.children('td.title > span.outline').text()
-            obj['genres'] = movie.children('td.title > span.genre').text().split(' | ')
-            obj['rated'] = movie.children('td.title > span.certificate > span').attr['title']
-            
-            try:
-                # get runtime info; could be missing
-                obj['runtime'] = int(movie.children('td.title > span.runtime').text().split()[0])
-            except:
-                obj['runtime'] = None
-            
-            try:
-                # get user rating info; could be missing
-                z = movie.children('td.title > div.user_rating > div.rating').attr['title']
-                z = z.split()
-                # normalize on a scale of 0 to 100
-                obj['rating'] = int(z[3][:-3].replace('.',''))
-                obj['numratings'] = int(z[4][1:].replace(',',''))
-            except:
-                obj['rating'] = None
-                obj['numratings'] = None
-            
-            #print obj
-            results.append(obj)
-            
+        results += parseIMDBlist(url)
 
     return results
-    #.map(lambda i,e: pq(this).text())
-
-##    results = []
-##
-##    for m in movies.items():
-##        thisobj = {}
-##        
-##        for i,e in enumerate(m.children().items()):
-##            text = e.text()
-##            
-##            if i == 0:
-##                thisobj['rank'] = int(text[:-1])
-##            elif i == 1:
-##                thisobj['rating'] = int(text[:-1])
-##            elif i == 2:
-##                thisobj['title'] = text
-##            elif i == 3:
-##                thisobj['numratings'] = int(text)
-##
-##        results.append(thisobj)
-##        
-##    return results
 
 if __name__ == "__main__":
-    a = getTop100ByYear(2015)
-    for b in a:
-        print b
+    import sys
+
+    try:
+        year_text = sys.argv[1]
+    except:
+        print 'Invalid number of arguments'
+        sys.exit(1)
+
+    try:
+        year = int(year_text)
+    except:
+        print 'Argument must be an integer'
+        sys.exit(1)
+
+    a = getTop100ByYear(year)
+    print json.dumps(a, indent=2, sort_keys=True)
